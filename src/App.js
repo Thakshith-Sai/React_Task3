fcbe799d3c1f0ab21af4fc665a90ca9ceecc6e37
R THAKSHITH SAI <thakshithsai2004@gmail.com>
2023-10-31 11:05:01 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import React from "react" ;
+
+import ColorPicker from './components/ColorPicker';
+
+function App() {
+  return (
+    <div className='outerCont'>
+      <h2>Color Picker </h2>
+    <div className='innerCont'>
+       <ColorPicker colors={["red", "orange", "yellow", "green", "blue", "purple", "pink", "brown", "black", "gray",]} />
+    </div>
+    </div>
+  );
+}
+
+export default App;
